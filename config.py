a9b6f41306f0f3aaebabdc2c3f51553f7f9e467f
sungjinw04 <abhishekpandit4224@gmail.com>
2024-08-08 12:28:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("25064357"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("cda9f1b3f9da4c0c93d1f5c23ccb19e2"")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("7306743441:AAESfANKNCBe4PRUawxrt-YeP8tFx0KijAc")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_USERNAME = os.environ.get("soulsguardianbot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_ID = int(os.environ.get("1886390680"))
 # ------------------X------------------------------
 # ------------------------------------------------
 LOGGER_ID = int(os.environ.get("LOGGER_ID"))
